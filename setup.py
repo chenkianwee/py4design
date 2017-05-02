1b1174c6f25ec4e18cc7b0ce307680edc33d5fa2
chenkianwee <chenkianwee@gmail.com>
2017-05-02 15:38:49 +0800

    successful deployment 0.1a4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = "Chen Kian Wee"
 __copyright__ = "Copyright 2016, Chen Kian Wee"
 __credits__ = ["Chen Kian Wee"]
 __license__ = "GPL3"
-__version__ = "0.1a1"
+__version__ = "0.1a4"
 __maintainer__ = "Chen Kian Wee"
 __email__ = "chenkianwee@gmail.com"
 __status__ = "Development"
@@ -13,12 +13,15 @@ INSTALL_REQUIRES = ['lxml', 'pyshp', 'numpy', 'pycollada', 'networkx', 'scikit-l
 
 setup(name='pyliburo',
       packages = find_packages(),
+      package_data={
+          'pyliburo': ['databases/ettv/*.csv'],
+          },
       version=__version__,
       description='Python Library for Urban Optimization',
       author=__author__,
       author_email=__email__,
       url='https://github.com/chenkianwee/pyliburo',
-      download_url = 'https://github.com/chenkianwee/pyliburo/archive/0.12.tar.gz',
+      download_url = 'https://github.com/chenkianwee/pyliburo/archive/0.12a.tar.gz',
       keywords = ["urban design", "architecture design", "design optimisation"],
       install_requires=INSTALL_REQUIRES,
       classifiers = ['Development Status :: 3 - Alpha',
